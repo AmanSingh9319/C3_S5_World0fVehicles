d11c31f51a87c8303469056c94de4f1518fe27c1
Aman Singh <singhaman4570@gmail.com>
2022-08-30 17:21:23 +0530

    added the override method

--- src/main/java/data/CVT.java
+++ src/main/java/data/CVT.java
@@ -5,5 +5,24 @@
  */
 package data;
 
-public class CVT {
+public class CVT extends Automatic {
+
+    public CVT(String transmissiontype, String modelNo, int noOfGears) {
+        super(transmissiontype, modelNo, noOfGears);
+    }
+
+    @Override
+    public double[] getGearRatio() {
+        return super.getGearRatio();
+    }
+
+    @Override
+    public void setGearRatio(double[] gearRatio) {
+        super.setGearRatio(gearRatio);
+    }
+
+    @Override
+    public void showSpecs() {
+        super.showSpecs();
+    }
 }
