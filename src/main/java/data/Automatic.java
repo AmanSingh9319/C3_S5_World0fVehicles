670600038bb4c2667268b98ad70640fcc2834f22
Aman Singh <singhaman4570@gmail.com>
2022-08-30 16:44:59 +0530

    in side of Automatic extends Transmission class and used override method

--- src/main/java/data/Automatic.java
+++ src/main/java/data/Automatic.java
@@ -5,5 +5,24 @@
  */
 package data;
 
-public class Automatic {
+public class Automatic extends Transmission {
+
+    public Automatic(String transmissiontype, String modelNo, int noOfGears) {
+        super(transmissiontype, modelNo, noOfGears);
+    }
+
+    @Override
+    public double[] getGearRatio() {
+        return super.getGearRatio();
+    }
+
+    @Override
+    public void setGearRatio(double[] gearRatio) {
+        super.setGearRatio(gearRatio);
+    }
+
+    @Override
+    public void showSpecs() {
+        super.showSpecs();
+    }
 }
